5e97ef1444197694a0550b06abc51b402d147968
jean-gvh <jean.guinvarch@gmail.com>
2024-03-08 16:10:10 +0100

    modified the backend orkflow

--- app.py
+++ app.py
@@ -2,11 +2,8 @@ from flask import Flask, render_template, request, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, joinedload
 from models import PokemonSet, EbaySalesData
-import os
-from google.cloud import storage
-import csv
-import functions
-import random
+from collections import defaultdict
+
 
 # Configuration de l'application Flask
 app = Flask(__name__, '/static')
@@ -32,7 +29,20 @@ def get_sales_data():
     # Effectuez les opérations nécessaires pour obtenir les données de ventes
     session = Session()
     pokemon_sales = session.query(EbaySalesData).options(joinedload('pokemon_set')).filter(EbaySalesData.card_name == pokemon_name).all()
-    session.close()
+
+    # Prétraitement des dates et calculs
+    grouped_data = defaultdict(lambda: {'prices': [], 'date': None})
+
+    for sale in pokemon_sales:
+        date_str = sale.sold_date.strftime('%Y-%m-%d')
+        grouped_data[date_str]['prices'].append(sale.card_price_EUR)
+        grouped_data[date_str]['date'] = date_str
+
+    # Calcul des moyennes
+    averaged_data = [{'date': group['date'], 'averagePrice': sum(group['prices']) / len(group['prices'])} for group in grouped_data.values()]
+
+    # Trie des données par date
+    averaged_data.sort(key=lambda x: x['date'])
 
     # Calcul des KPIs
     num_sales = len(pokemon_sales)
@@ -42,11 +52,14 @@ def get_sales_data():
 
     kpis = {'num_sales': num_sales, 'avg_price': avg_price, 'max_price': max_price, 'min_price': min_price}
 
-    # Retournez les données au format JSON
+    session.close()
+
+    # Retournez les données au format JSON, y compris les données prétraitées
     return jsonify({
         'pokemon_name': pokemon_name,
         'sales': [{'card_name': sale.card_name, 'card_price_EUR': sale.card_price_EUR, 'sold_date': sale.sold_date, 'seller_ID': sale.seller_ID, 'pokemon_set': {'set_name': sale.pokemon_set.set_name}} for sale in pokemon_sales],
         'kpis': kpis,
+        'chartData': averaged_data  # Ajoutez les données prétraitées ici
     })
 
 if __name__ == '__main__':
